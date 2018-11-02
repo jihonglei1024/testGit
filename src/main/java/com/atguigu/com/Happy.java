8b58e9380d9f5d101c8a563ac5b243ca173c3a04
eclipse_user <eclipe_user@atguigu.com>
2018-11-02 17:23:30 +0800

    sddfsdfasdfsa

--- src/main/java/com/atguigu/com/Happy.java
+++ src/main/java/com/atguigu/com/Happy.java
@@ -4,6 +4,9 @@ public class Happy {
 	
 	public static void main(String[] args) {
 		System.out.println("test1....");
+		System.out.println("test2...");
+		System.out.println("test3...");
+		System.out.println("test4...");
 	}
 	
 	
